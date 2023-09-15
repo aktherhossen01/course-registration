9c6bde41afc93bca14b45fccd4c13978a0cca9cc
aktherhossen01 <mdakterhossen122@gmail.com>
2023-09-15 18:40:43 +0600

    course registation project card and calculation item compleate

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({bookmark,credit,remaining}) => {
     let count = 1
     console.log(bookmark);
     return (
-        <div className="shadow-lg p-4 text-left border-2 w-[384px]">
+        <div className="shadow-lg p-4 text-left border-2 rounded-lg w-[384px]">
             <h1 className="mb-2 text-2xl font-bold">Credit Hour Remaining: {remaining} hr</h1>
            <div className="bg-[#FFF] mb-3"> <hr />
            <h1 className="text-2xl font-semibold mb-4">Course Name</h1>
